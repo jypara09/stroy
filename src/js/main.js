f78a45801e55ec4b85b43a21c02ee96af88c733f
jypara <jypar09_12@icloud.com>
2023-05-03 19:58:58 +0600

    инициализация проекта

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 1,
+    });
+    // Go to the next item
+    $('.customNextBtn').click(function () {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.customPrevBtn').click(function () {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+});
